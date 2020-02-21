5cd768d4b75d90c7b3dce8ed79b9c38ef0edf07d
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2020-02-21 21:43:34 +0100

    Initial commit

---
+++ src/main/kotlin/com/decagon/recipeapi/RecipeApiApplication.kt
@@ -0,0 +1,11 @@
+package com.decagon.recipeapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecipeApiApplication>(*args)
+}
